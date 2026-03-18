bcd4070c30954f66c72c6b88ecfe5e9119bc27d3
hibamarzak <hibamarzak10@gmail.com>
2026-03-18 07:29:07 +0000

    Implement User class with attributes and methods

--- src/model/User.java
+++ src/model/User.java
@@ -1,1 +1,67 @@
+package model;
 
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private String adresse;
+    private String telephone;
+    private boolean actif;
+
+    public User() {}
+
+    public User(int id, String username, String email, String password, String role) {
+        setId(id);
+        setUsername(username);
+        setEmail(email);
+        setPassword(password);
+        setRole(role);
+        setActif(true);
+    }
+
+    public String getNomComplet() {
+        return username;
+    }
+
+    public boolean verifierMotDePasse(String inputPassword) {
+        return inputPassword != null && inputPassword.equals(getPassword());
+    }
+
+    public void modifierProfil(String email, String telephone, String adresse) {
+        setEmail(email);
+        setTelephone(telephone);
+        setAdresse(adresse);
+    }
+
+    // Getters & Setters
+    public int getId() { return id; }
+    public void setId(int newId) { id = newId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String newUsername) { username = newUsername; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String newEmail) { email = newEmail; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String newPassword) { password = newPassword; }
+
+    public String getRole() { return role; }
+    public void setRole(String newRole) { role = newRole; }
+
+    public String getAdresse() { return adresse; }
+    public void setAdresse(String newAdresse) { adresse = newAdresse; }
+
+    public String getTelephone() { return telephone; }
+    public void setTelephone(String newTelephone) { telephone = newTelephone; }
+
+    public boolean isActif() { return actif; }
+    public void setActif(boolean newActif) { actif = newActif; }
+
+    @Override
+    public String toString() {
+       return "User{id=" + getId() + ", username='" + getUsername() + "', email='" + getEmail() + "', role='" + getRole() + "', actif=" + isActif() + '}';
+    }
+}
